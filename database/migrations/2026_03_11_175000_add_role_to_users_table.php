c5298aef46329222f9ccb0c7e39fed98e68f7f33
jesuiskoriel <lajimijhawad@gmail.com>
2026-03-13 11:52:41 +0100

    update

--- database/migrations/2026_03_11_175000_add_role_to_users_table.php
+++ database/migrations/2026_03_11_175000_add_role_to_users_table.php
@@ -1,6 +1,6 @@
 <?php
 /* METAL-EXPLAIN: Ce fichier fait une partie du boulot de l'app bibliothèque. 
- * Version simple: ce fichier sert à éviter que tout parte en spaghetti 😄.
+ * Version simple: ce fichier sert à éviter que tout parte en spaghetti .
  * Lisez les fonctions une par une: chacune fait un mini boulot précis.
  */
 
